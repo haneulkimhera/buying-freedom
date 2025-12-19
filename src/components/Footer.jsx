b53eaf2edaecde7f1310f4026f3f6f16b0a2110e
haneulkimhera <haneulkimhera@gmail.com>
2025-12-19 18:24:01 +0900

    REFACTOR: 연락처 정보 삭제

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -148,7 +148,7 @@ const Footer = () => {
                   strokeLinejoin="round"
                 />
               </svg>
-              <div className="contact-text">경상남도 창원시 성산구 원이대로 863번길 19<br />203동 2004호</div>
+              {/* <div className="contact-text"><br /></div> */}
             </div>
             <div className="contact-item">
               <svg
@@ -167,7 +167,7 @@ const Footer = () => {
                   strokeLinejoin="round"
                 />
               </svg>
-              <div className="contact-text">010-3284-0425</div>
+              {/* <div className="contact-text"><br /></div> */}
             </div>
             <div className="contact-item">
               <svg
